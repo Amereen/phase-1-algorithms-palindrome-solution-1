504f749bd1ab98ecdd33cc843033545332476cc4
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-16 16:33:22 -0400

    commit tree 0cc4153a287b36eea6920f67640013d08495adf2

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word){
+  return word.split('').reverse().join('')
+
 }
+function isPalindrome(word) {
+const reversedWord = reverse(word)
+  return reversedWord === word
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+  
+} 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
